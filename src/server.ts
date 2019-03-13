163caf3b36de150eda5573d28554218f50f6edf8
Julien Sié <julien.sie@gmail.com>
2019-03-13 18:18:26 +0100

    response the firebase creds as json

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import creds from './creds/serviceAccountKey.json';
 const app: express.Application = express();
 
 // const creds = require();
-app.get('/',(req,res) => res.send(`Hello MotherFuckers\n${JSON.stringify(creds)}`));
+app.get('/',(req,res) => res.json(creds));
 
 const port = process.env.PORT || 9000;
 app.listen(port, ()=>console.log(`Listening on port ${port}`));
