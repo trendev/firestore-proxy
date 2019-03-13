7bb89a63fa5fee35ba8663573f3b2787fa6a9d3c
Julien Sié <julien.sie@gmail.com>
2019-03-13 18:50:58 +0100

    type the firebase credentials

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express';
-//@ts-ignore
 import creds from './creds/serviceAccountKey.json';
+
 const app: express.Application = express();
 
-// const creds = require();
-app.get('/',(req,res) => res.json(creds));
+app.get('/', (req, res) => res.json(creds));
 
 const port = process.env.PORT || 9000;
-app.listen(port, ()=>console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
