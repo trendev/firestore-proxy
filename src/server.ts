0bd0d5fd182255ec3095866a081799e5b10200e5
Julien Sié <julien.sie@gmail.com>
2019-03-14 17:50:30 +0100

    use bodyparser

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
-
 import creds from './creds/serviceAccountKey.json';
+import bodyParser from 'body-parser';
 
 const app: express.Application = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.json(creds));
 
