f7f20f9eb440f2b5b6e3c3073f0d367ee40d00f6
Julien Sié <julien.sie@gmail.com>
2019-03-14 17:12:48 +0100

    use morgan

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import morgan from 'morgan';
+
 import creds from './creds/serviceAccountKey.json';
 
 const app: express.Application = express();
 
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => res.json(creds));
 
 const port = process.env.PORT || 9000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Server listening on port ${port}`));
