003e710758d2089ea1cc24dc7c2414ce4dbafb28
Julien SIE <julien.sie@gmail.com>
2019-03-21 16:54:54 +0100

    inits route impl.

--- src/controllers/jwt-revoked-set-controller.ts
+++ src/controllers/jwt-revoked-set-controller.ts
@@ -11,10 +11,31 @@ export class JWTRevokedSetController extends JWTAbstractController {
     }
 
     private init() {
-        this.Router().get("/", this.getAll);
+        this.Router()
+            .get("/", this.getAll)
+            .delete("/bulk-removes", this.bulkRemoves)
+            .post("/", this.create)
+            .post("/bulk-creation", this.bulkCreation)
+            .delete("/:token", this.delete);
     }
 
     private getAll = (req: Request, res: Response, next: NextFunction) => {
         res.json([]);
+    }
+
+    private bulkRemoves = (req: Request, res: Response, next: NextFunction) => {
+        res.json(req.body);
+    }
+
+    private create = (req: Request, res: Response, next: NextFunction) => {
+        res.status(201).json(req.body);
+    }
+
+    private bulkCreation = (req: Request, res: Response, next: NextFunction) => {
+        res.status(201).json(req.body);
+    }
+
+    private delete = (req: Request, res: Response, next: NextFunction) => {
+        res.sendStatus(200);
     }
 }
--- src/controllers/jwt-white-map-controller.ts
+++ src/controllers/jwt-white-map-controller.ts
@@ -11,10 +11,36 @@ export class JWTWhiteMapController extends JWTAbstractController {
     }
 
     private init() {
-        this.Router().get("/", this.getAll);
+        this.Router()
+            .get("/", this.getAll)
+            .put("/bulk-updates", this.bulkUpdates)
+            .delete("/bulk-removes", this.bulkRemoves)
+            .post("/", this.create)
+            .put("/", this.update)
+            .delete("/:email", this.delete);
     }
 
     private getAll = (req: Request, res: Response, next: NextFunction) => {
         res.json([]);
+    }
+
+    private bulkUpdates = (req: Request, res: Response, next: NextFunction) => {
+        res.json(req.body);
+    }
+
+    private bulkRemoves = (req: Request, res: Response, next: NextFunction) => {
+        res.json(req.body);
+    }
+
+    private create = (req: Request, res: Response, next: NextFunction) => {
+        res.status(201).json(req.body);
+    }
+
+    private update = (req: Request, res: Response, next: NextFunction) => {
+        res.json(req.body);
+    }
+
+    private delete = (req: Request, res: Response, next: NextFunction) => {
+        res.sendStatus(200);
     }
 }
